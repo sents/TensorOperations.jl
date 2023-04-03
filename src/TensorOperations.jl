bcf1b3cc926719c544749f4aeeb82449d518cfda
Finn Krein <finn@krein.moe>
2023-04-03 11:24:20 +0200

    Change to using CUDA 4

--- src/TensorOperations.jl
+++ src/TensorOperations.jl
@@ -144,31 +144,33 @@ function __init__()
     resize!(cache; maxsize = default_cache_size())
 
     @require CUDA="052768ef-5323-5732-b1bb-66c8b64840ba" begin
-        if CUDA.functional() && CUDA.has_cutensor()
-            const CuArray = CUDA.CuArray
-            const CublasFloat = CUDA.CUBLAS.CublasFloat
-            const CublasReal = CUDA.CUBLAS.CublasReal
-            for s in (:handle, :CuTensorDescriptor, :cudaDataType,
-                    :cutensorContractionDescriptor_t, :cutensorContractionFind_t,
-                    :cutensorContractionPlan_t,
-                    :CUTENSOR_OP_IDENTITY, :CUTENSOR_OP_CONJ, :CUTENSOR_OP_ADD,
-                    :CUTENSOR_ALGO_DEFAULT,  :CUTENSOR_WORKSPACE_RECOMMENDED,
-                    :cutensorPermutation, :cutensorElementwiseBinary, :cutensorReduction,
-                    :cutensorReductionGetWorkspace, :cutensorComputeType,
-                    :cutensorGetAlignmentRequirement, :cutensorInitContractionDescriptor,
-                    :cutensorInitContractionFind, :cutensorContractionGetWorkspace,
-                    :cutensorInitContractionPlan, :cutensorContraction)
-                eval(:(const $s = CUDA.CUTENSOR.$s))
+        @require cuTENSOR="011b41b2-24ef-40a8-b3eb-fa098493e9e1" begin
+            if CUDA.functional() && cuTENSOR.has_cutensor()
+                const CuArray = CUDA.CuArray
+                const CublasFloat = CUDA.CUBLAS.CublasFloat
+                const CublasReal = CUDA.CUBLAS.CublasReal
+                for s in (:handle, :CuTensorDescriptor, :cudaDataType_t,
+                        :cutensorContractionDescriptor_t, :cutensorContractionFind_t,
+                        :cutensorContractionPlan_t,
+                        :CUTENSOR_OP_IDENTITY, :CUTENSOR_OP_CONJ, :CUTENSOR_OP_ADD,
+                        :CUTENSOR_ALGO_DEFAULT,  :CUTENSOR_WORKSPACE_RECOMMENDED,
+                        :cutensorPermutation, :cutensorElementwiseBinary, :cutensorReduction,
+                        :cutensorReductionGetWorkspace, :cutensorComputeType,
+                        :cutensorGetAlignmentRequirement, :cutensorInitContractionDescriptor,
+                        :cutensorInitContractionFind, :cutensorContractionGetWorkspace,
+                        :cutensorInitContractionPlan, :cutensorContraction)
+                    eval(:(const $s = cuTENSOR.$s))
+                end
+                if isdefined(CUDA, :default_stream)
+                    const default_stream = CUDA.default_stream
+                else
+                    const default_stream = CUDA.CuDefaultStream
+                end
+                include("implementation/cuarray.jl")
+                @nospecialize
+                include("indexnotation/cutensormacros.jl")
+                @specialize
             end
-            if isdefined(CUDA, :default_stream)
-                const default_stream = CUDA.default_stream
-            else
-                const default_stream = CUDA.CuDefaultStream
-            end
-            include("implementation/cuarray.jl")
-            @nospecialize
-            include("indexnotation/cutensormacros.jl")
-            @specialize
         end
     end
 end
