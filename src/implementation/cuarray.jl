c1eac52e7bcab042edebf5d7e77426c3677309fc
Finn Krein <finn@krein.moe>
2023-04-03 11:24:20 +0200

    Change to using CUDA 4

--- src/implementation/cuarray.jl
+++ src/implementation/cuarray.jl
@@ -29,7 +29,7 @@ function add!(α, A::CuArray{<:Any,N}, CA::Symbol,
     descA = CuTensorDescriptor(A; op=opA)
     descC = CuTensorDescriptor(C; op=CUTENSOR_OP_IDENTITY)
     descD = descC
-    typeCompute = convert(cudaDataType, T)
+    typeCompute = convert(cudaDataType_t, T)
     modeA = collect(Cint, 1:N)
     modeC = collect(Cint, indCinA)
     stream = default_stream()
